585a9aaa06e574dd9ec68716c59f5469722a0b0d
Marcelo Ferreira Mota Júnior <88806962+mfmjr@users.noreply.github.com>
2022-09-24 17:03:32 -0300

    quasela

--- script.js
+++ script.js
@@ -13,8 +13,9 @@ const showCard = (data) => {
   const studentsEl = document.createElement("div");
 
   studentsEl.innerHTML = `
-    <div class="main-card">
-          <div class="cover-photo"></div>
+   
+  <div class="main-card">
+       <div class="cover-photo"></div>
           <div class="photo">
             <img
               src="${data[index].img}"
@@ -25,36 +26,24 @@ const showCard = (data) => {
           <div class="content">
             <h2 class="name">${data[index].name}</h2>
             <h3 class="fullstack">
+            <div class="team">Turma 22.1</div>
               Full Stack Development<br />
               <a
-                href="https://www.youracclaim.com/badges/e678fb12-49ea-4609-8d97-622d7416880d"
+                href=""
                 class="certified"
                 target="_blank"
-                >Somos Tera</a
+                >
+               </a
               >
             </h3>
-            <h3 class="">
-              <a href="mailto:abbassi.abdeladhim31791@gmail.com"
-                >abbassi.abdeladhim31791@gmail.com</a
-              >
-            </h3>
+            
+              <div class="about">
+                ${data[index].about}</div>              
+            
           </div>
-        </div>
-        <div class="contact">
-              <ul>
-                  <a href="${data[index].linkedin}" target="_blank">
-                      <i class="fa fa-linkedin"></i>
-                  </a>
-                  <a href="${data[index].github}" target="_blank">
-                      <i class="fa fa-github"></i>
-                  </a>
-                  <a href="https://codepen.io/Adhouma/" target="_blank">
-                      <i class="fa fa-codepen"></i>
-                  </a>
-              </ul>
-          </div>
+        </div>        
       </div>
-        `;
+       `;
 
   container.appendChild(studentsEl);
 };
